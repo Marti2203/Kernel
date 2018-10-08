67a953e684f2269772e45ee1f42a6436ee5bee10
Martin Mirchev <marti_2203@abv.bg>
2018-10-08 22:16:52 +0300

    Added continuations

---
+++ Kernel/BaseTypes/Continuation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kernel
+{
+    public class Continuation : Object
+    {
+        public static readonly Continuation Base = new Continuation();
+
+        public Continuation Parent { get; private set; }
+        public Func<Object, Object> Evaluation { get; private set; }
+        Continuation()
+        {
+
+        }
+        public Continuation(Continuation parent, Func<Object, Object> evaluation)
+        {
+            Parent = parent;
+            Evaluation = evaluation;
+        }
+        public Object Invoke(Object value) => Evaluation(value);
+        public override string ToString() => "Continuation";
+        public override bool Equals(Object other) => ReferenceEquals(this, other);
+    }
+}
